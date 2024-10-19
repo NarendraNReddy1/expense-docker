7e246e0a0578e63d160d4a078a6e1d923fec728f
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-10-19 12:25:07 -0700

    ALL

--- install-docker.sh
+++ install-docker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 R="\e[31m"
 N="\e[0m"
-yum install -y yum-utils
-yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ec2-user
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+ysudo um install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
 sudo growpart /dev/nvme0n1 4
 sudo lvextend -l +50%FREE /dev/RootVG/rootVol
 sudo lvextend -l +50%FREE /dev/RootVG/varVol
